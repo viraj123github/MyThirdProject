b5bd9ca08bc1380d8a78e4d02db33594b56bd6ec
viraj123github <virajphalke20@gmail.com>
2024-05-29 23:34:57 +0530

    Update Example3.java

--- src/com/program1/Example3.java
+++ src/com/program1/Example3.java
@@ -5,6 +5,7 @@ public class Example3 {
 	public static void main(String[] args) {
 		System.out.println("example33 class");
                 System.out.println("example changes now");
+		System.out.println("changes done");
 	}
 
 }
