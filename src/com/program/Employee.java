8c153f89bb3faead2d8fd1c18979ef3e2e031a7a
viraj phalke <virajphalke20@gmail.com>
2024-05-30 00:29:19 +0530

    added changes to task1

--- src/com/program/Employee.java
+++ src/com/program/Employee.java
@@ -4,13 +4,7 @@ public class Employee {
 	public static void main(String[] args) {
 
 		System.out.println("i am from employee clas");
-		System.out.println("changes done at 12.23pm");
-		System.out.println("i am from employee class");
-		System.out.println("changes done at 12.22pm");
-		System.out.println("changes done at 10.40pm");
-		System.out.println("changes at 23.07");
-		System.out.println("changes done at 23.17pm");
-		System.out.println("changes at 23.58");
+		System.out.println("hi");
 		
 
 	}
