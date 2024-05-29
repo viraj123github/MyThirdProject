6ebe4f974863ca0294d8e292fdd0075b4d482445
viraj123github <virajphalke20@gmail.com>
2024-05-29 12:22:46 +0530

    Update Employee.java

--- src/com/program/Employee.java
+++ src/com/program/Employee.java
@@ -3,6 +3,7 @@ package com.program;
 public class Employee {
 	public static void main(String[] args) {
 		System.out.println("i am from employee class");
+		System.out.println("changes done at 12.22pm");
 	}
 
 }
