d87cdbb889a7ecbd578dd3cda74b68a876a9c5b5
viraj phalke <virajphalke20@gmail.com>
2024-05-29 12:28:35 +0530

    added dev-2 commit

--- src/com/program/Employee.java
+++ src/com/program/Employee.java
@@ -2,7 +2,8 @@ package com.program;
 
 public class Employee {
 	public static void main(String[] args) {
-		System.out.println("i am from employee class");
+		System.out.println("i am from employee clas");
+		System.out.println("changes done at 12.23pm");
 	}
 
 }
--- src/com/program/Person.java
+++ src/com/program/Person.java
@@ -3,7 +3,8 @@ package com.program;
 public class Person {
 
 	public static void main(String[] args) {
-	System.out.println("i am from personal class");
+	System.out.println("i am from personal clas");
+	System.out.println("changes done at 12.23p.m");
 
 	}
 
