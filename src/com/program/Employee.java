5179f799e18f9f9b712db99b9ade0290770c12a7
viraj123github <virajphalke20@gmail.com>
2024-05-30 09:35:18 +0530

    Update Employee.java

--- src/com/program/Employee.java
+++ src/com/program/Employee.java
@@ -4,6 +4,8 @@ public class Employee {
 	public static void main(String[] args) {
 		System.out.println("i am from employee class");
 		System.out.println("changes in master");
+		System.out.println("changes done now);
+		System.out.println("now everything is fine");
 	}
 
 }
